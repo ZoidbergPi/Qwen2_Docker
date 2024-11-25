3ac48b3b1e2ac2514a3ff9b393882acb5ffb32d6
Zoidberg Pi <ZoidbergPi@users.noreply.github.com>
2024-11-25 18:30:23 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-MODELSCOPE_CACHE='qwen2-7b-instruct' python -m vllm.entrypoints.openai.api_server \
-    --model qwen/Qwen2-7B-Instruct --dtype=half --max-model-len 8192  --gpu-memory-utilization 0.95
-wait
